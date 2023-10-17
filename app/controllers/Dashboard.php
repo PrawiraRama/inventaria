a61f593c8b0de71b3450408f23e205e38be5b492
PrawiraRama <yudhaprawirarama12@gmail.com>
2023-10-17 15:40:31 +0800

    memperbarui Dashboard

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -6,8 +6,8 @@ class Dashboard extends Controller{
     {
         $data['judul'] = 'Dashboard';
 
-        $this->view('tamplates/header', $data);
-        $this->view('dashboard/index');
+        $this->view('tamplates/header1', $data);
+        $this->view('dashboard1/index');
         $this->view('tamplates/footer');
     }
 }
---
+++ app/view/tamplates/header1.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Halaman <?= $data['judul']; ?></title>
+      <link href="<?= BASEURL ?>/css/bootstrap.min.css" rel="stylesheet">
+      <link rel="stylesheet" href="<?= BASEURL ?>/css/merge.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    </head>
+    <body>
